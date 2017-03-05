a95d27ac6571a71b0c1966cf2394223d635fa42d
Johan Wänglöf <jwanglof@gmail.com>
2017-03-05 10:40:34 +0100

    Css fuck-up. Fixed

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,1 +1,179 @@
-!function(){"use strict";var e="undefined"==typeof global?self:global;if("function"!=typeof e.require){var n={},r={},t={},i={}.hasOwnProperty,o=/^\.\.?(\/|$)/,a=function(e,n){for(var r,t=[],i=(o.test(n)?e+"/"+n:n).split("/"),a=0,u=i.length;a<u;a++)r=i[a],".."===r?t.pop():"."!==r&&""!==r&&t.push(r);return t.join("/")},u=function(e){return e.split("/").slice(0,-1).join("/")},l=function(n){return function(r){var t=a(u(n),r);return e.require(t,n)}},c=function(e,n){var t=g&&g.createHot(e),i={id:e,exports:{},hot:t};return r[e]=i,n(i.exports,l(e),i),i.exports},f=function(e){return t[e]?f(t[e]):e},s=function(e,n){return f(a(u(e),n))},p=function(e,t){null==t&&(t="/");var o=f(e);if(i.call(r,o))return r[o].exports;if(i.call(n,o))return c(o,n[o]);throw new Error("Cannot find module '"+e+"' from '"+t+"'")};p.alias=function(e,n){t[n]=e};var d=/\.[^.\/]+$/,v=/\/index(\.[^\/]+)?$/,_=function(e){if(d.test(e)){var n=e.replace(d,"");i.call(t,n)&&t[n].replace(d,"")!==n+"/index"||(t[n]=e)}if(v.test(e)){var r=e.replace(v,"");i.call(t,r)||(t[r]=e)}};p.register=p.define=function(e,t){if(e&&"object"==typeof e)for(var o in e)i.call(e,o)&&p.register(o,e[o]);else n[e]=t,delete r[e],_(e)},p.list=function(){var e=[];for(var r in n)i.call(n,r)&&e.push(r);return e};var g=e._hmr&&new e._hmr(s,p,n,r);p._cache=r,p.hmr=g&&g.wrap,p.brunch=!0,e.require=p}}(),function(){"undefined"==typeof window?this:window;require.register("js/initialize.js",function(e,n,r){"use strict";document.addEventListener("DOMContentLoaded",function(){console.log("Initialized app");var e=n("smooth-scroll");e.init({offset:85,selectorHeader:"navbar"})}),Zepto(function(e){console.log("Zepto initialized!"),e(".gmap-container").mouseleave(function(){e(".gmap-container iframe").css("pointer-events","none")})})}),require.register("___globals___",function(e,n,r){})}(),require("___globals___");
+(function() {
+  'use strict';
+
+  var globals = typeof window === 'undefined' ? global : window;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+  var aliases = {};
+  var has = ({}).hasOwnProperty;
+
+  var expRe = /^\.\.?(\/|$)/;
+  var expand = function(root, name) {
+    var results = [], part;
+    var parts = (expRe.test(name) ? root + '/' + name : name).split('/');
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function expanded(name) {
+      var absolute = expand(dirname(path), name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var hot = null;
+    hot = hmr && hmr.createHot(name);
+    var module = {id: name, exports: {}, hot: hot};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var expandAlias = function(name) {
+    return aliases[name] ? expandAlias(aliases[name]) : name;
+  };
+
+  var _resolve = function(name, dep) {
+    return expandAlias(expand(dirname(name), dep));
+  };
+
+  var require = function(name, loaderPath) {
+    if (loaderPath == null) loaderPath = '/';
+    var path = expandAlias(name);
+
+    if (has.call(cache, path)) return cache[path].exports;
+    if (has.call(modules, path)) return initModule(path, modules[path]);
+
+    throw new Error("Cannot find module '" + name + "' from '" + loaderPath + "'");
+  };
+
+  require.alias = function(from, to) {
+    aliases[to] = from;
+  };
+
+  var extRe = /\.[^.\/]+$/;
+  var indexRe = /\/index(\.[^\/]+)?$/;
+  var addExtensions = function(bundle) {
+    if (extRe.test(bundle)) {
+      var alias = bundle.replace(extRe, '');
+      if (!has.call(aliases, alias) || aliases[alias].replace(extRe, '') === alias + '/index') {
+        aliases[alias] = bundle;
+      }
+    }
+
+    if (indexRe.test(bundle)) {
+      var iAlias = bundle.replace(indexRe, '');
+      if (!has.call(aliases, iAlias)) {
+        aliases[iAlias] = bundle;
+      }
+    }
+  };
+
+  require.register = require.define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has.call(bundle, key)) {
+          require.register(key, bundle[key]);
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+      delete cache[bundle];
+      addExtensions(bundle);
+    }
+  };
+
+  require.list = function() {
+    var list = [];
+    for (var item in modules) {
+      if (has.call(modules, item)) {
+        list.push(item);
+      }
+    }
+    return list;
+  };
+
+  var hmr = globals._hmr && new globals._hmr(_resolve, require, modules, cache);
+  require._cache = cache;
+  require.hmr = hmr && hmr.wrap;
+  require.brunch = true;
+  globals.require = require;
+})();
+
+(function() {
+var global = window;
+var __makeRelativeRequire = function(require, mappings, pref) {
+  var none = {};
+  var tryReq = function(name, pref) {
+    var val;
+    try {
+      val = require(pref + '/node_modules/' + name);
+      return val;
+    } catch (e) {
+      if (e.toString().indexOf('Cannot find module') === -1) {
+        throw e;
+      }
+
+      if (pref.indexOf('node_modules') !== -1) {
+        var s = pref.split('/');
+        var i = s.lastIndexOf('node_modules');
+        var newPref = s.slice(0, i).join('/');
+        return tryReq(name, newPref);
+      }
+    }
+    return none;
+  };
+  return function(name) {
+    if (name in mappings) name = mappings[name];
+    if (!name) return;
+    if (name[0] !== '.' && pref) {
+      var val = tryReq(name, pref);
+      if (val !== none) return val;
+    }
+    return require(name);
+  }
+};
+require.register("js/initialize.js", function(exports, require, module) {
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  // do your setup here
+  console.log('Initialized app');
+
+  var smoothScroll = require('smooth-scroll');
+
+  smoothScroll.init({
+    offset: 85,
+    selectorHeader: 'navbar'
+  });
+});
+
+Zepto(function ($) {
+  console.log('Zepto initialized!');
+  $(".gmap-container").mouseleave(function () {
+    $('.gmap-container iframe').css("pointer-events", "none");
+  });
+});
+});
+
+require.register("___globals___", function(exports, require, module) {
+  
+});})();require('___globals___');
+
+
+//# sourceMappingURL=app.js.map
